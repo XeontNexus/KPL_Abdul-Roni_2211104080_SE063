189b96235a69d945f76b2ad05baeb3a2054a4926
abdulroni31 <abdulroni0616@gmail.com>
2025-05-05 15:13:44 +0700

    commit ulang pertemuan 2

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJs/variable-local-global.js
@@ -0,0 +1,12 @@
+let globalVarible = "Abdul Roni";
+
+function getMyIdentity() {
+  let localVariable = 2211104080;
+
+  console.log(`Nama saya ${globalVarible} nim ${localVariable}`);
+}
+
+console.log(globalVarible);
+// console.log(localVariable)
+
+getMyIdentity();
---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJs/variable.js
@@ -0,0 +1,15 @@
+var name = "Abdul Roni";
+const nim = 2211104080;
+
+console.log(name);
+console.log(nim);
+
+console.log("nama saya adalah " + name + " dengan NIM " + nim);
+console.log(`nama saya adalah ${name} dengan NIM ${nim}`);
+
+console.log(`Nama saya adalah ${name}
+    nim saya adalah ${nim}`);
+
+var name = "Abdul";
+
+let fullName = "Roni";
