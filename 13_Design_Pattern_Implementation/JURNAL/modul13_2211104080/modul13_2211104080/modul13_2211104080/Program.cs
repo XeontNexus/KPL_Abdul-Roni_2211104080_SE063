7add0a6cde070e8836afdffd78401e17446a3434
Muhammad Rizaldy Akbar <muribar9@gmail.com>
2025-06-06 19:12:49 +0700

    wlewlewle

---
+++ 13_Design_Pattern_Implementation/JURNAL/modul13_2211104080/modul13_2211104080/modul13_2211104080/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class PusatDataSingleton
+{
+    private static PusatDataSingleton _instance;
+    public List<string> DataTersimpan { get; set; }
+
+    // Konstruktor privat untuk mencegah pembuatan instance secara langsung
+    private PusatDataSingleton()
+    {
+        DataTersimpan = new List<string>();
+    }
+
+    // Mendapatkan instance tunggal dari kelas PusatDataSingleton
+    public static PusatDataSingleton GetDataSingleton()
+    {
+        if (_instance == null)
+        {
+            _instance = new PusatDataSingleton();
+        }
+        return _instance;
+    }
+
+    // Mengembalikan seluruh data yang tersimpan
+    public List<string> GetSemuaData()
+    {
+        return DataTersimpan;
+    }
+
+    // Menampilkan seluruh data
+    public void PrintSemuaData()
+    {
+        foreach (var data in DataTersimpan)
+        {
+            Console.WriteLine(data);
+        }
+    }
+
+    // Menambahkan data baru ke dalam list
+    public void AddSebuahData(string input)
+    {
+        DataTersimpan.Add(input);
+    }
+
+    // Menghapus data berdasarkan index yang diberikan
+    public void HapusSebuahData(int index)
+    {
+        if (index >= 0 && index < DataTersimpan.Count)
+        {
+            DataTersimpan.RemoveAt(index);
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Membuat dua variabel dengan tipe PusatDataSingleton
+        PusatDataSingleton data1 = PusatDataSingleton.GetDataSingleton();
+        PusatDataSingleton data2 = PusatDataSingleton.GetDataSingleton();
+
+        // Menambahkan data anggota kelompok dan asisten praktikum ke data1
+        data1.AddSebuahData("Anggota 1");
+        data1.AddSebuahData("Anggota 2");
+        data1.AddSebuahData("Asisten Praktikum");
+
+        // Mencetak semua data dari data2
+        Console.WriteLine("Data di data2:");
+        data2.PrintSemuaData();
+
+        // Menghapus nama asisten praktikum
+        data2.HapusSebuahData(2);  // Indeks asisten praktikum adalah 2
+
+        // Mencetak data setelah penghapusan dari data1
+        Console.WriteLine("\nData di data1 setelah penghapusan:");
+        data1.PrintSemuaData();
+
+        // Mencetak jumlah data dari data1 dan data2
+        Console.WriteLine("\nJumlah data di data1: " + data1.GetSemuaData().Count);
+        Console.WriteLine("Jumlah data di data2: " + data2.GetSemuaData().Count);
+    }
+}
---
+++ 13_Design_Pattern_Implementation/TP/tpmodul13_2211104080/tpmodul13_2211104080/tpmodul13_2211104080/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul13_2211104065
+{
+    // Subject (yang akan diamati)
+    public class Subject
+    {
+        private List<IObserver> observers = new List<IObserver>();
+
+        // Menambahkan Observer
+        public void Attach(IObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        // Menghapus Observer
+        public void Detach(IObserver observer)
+        {
+            observers.Remove(observer);
+        }
+
+        // Memberitahukan Observer tentang perubahan
+        public void Notify()
+        {
+            foreach (var observer in observers)
+            {
+                observer.Update();
+            }
+        }
+    }
+
+    // Observer (pengamat)
+    public interface IObserver
+    {
+        void Update();
+    }
+
+    // ConcreteObserver (pengamat konkret)
+    public class ConcreteObserver : IObserver
+    {
+        private string _name;
+        private Subject _subject;
+
+        public ConcreteObserver(string name, Subject subject)
+        {
+            _name = name;
+            _subject = subject;
+            _subject.Attach(this);
+        }
+
+        public void Update()
+        {
+            Console.WriteLine($"Observer {_name} has been notified.");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Membuat instance Subject
+            var subject = new Subject();
+
+            // Membuat Observer
+            var observer1 = new ConcreteObserver("Observer 1", subject);
+            var observer2 = new ConcreteObserver("Observer 2", subject);
+
+            // Memberitahukan Observer tentang perubahan pada Subject
+            Console.WriteLine("Notifying observers...");
+            subject.Notify();
+
+            // Menghapus observer
+            subject.Detach(observer1);
+
+            // Memberitahukan Observer yang tersisa
+            Console.WriteLine("Notifying observers after detaching Observer 1...");
+            subject.Notify();
+        }
+    }
+}
